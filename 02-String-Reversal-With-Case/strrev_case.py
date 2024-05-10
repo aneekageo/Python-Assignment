b58559cbbc065b86d1f19aa0a5e86f35345684e4
Aneeka Geo <aneeka.geo@experionglobal.local>
2024-05-10 13:00:19 +0530

    String Reverse with case

--- 02-String-Reversal-With-Case/strrev_case.py
+++ 02-String-Reversal-With-Case/strrev_case.py
@@ -1,26 +1,3 @@
-# string = input("Enter a string: ")
-
-# res = []
-
-# for i in range(0,len(string)):
-#     if(string[i].isupper()):
-#         res.append(i)
-
-
-# reversed_str = string[::-1].lower()
-
-
-
-# new_string = ''
-# for i in range(len(reversed_str)):
-#     if i in res:
-#         new_string += reversed_str[i].upper()
-#     else:
-#         new_string += reversed_str[i]
-
-# print(new_string)
-
-
 string = input("Enter a string: ")
 string_split = string.split()
 result = []
